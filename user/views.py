833f394da65adedbd7b854a8a6c1ddfffe178375
Vadym Sulim <ohnooq1@gmail.com>
2024-12-26 01:59:15 +0200

    added views and serializers for user

--- user/views.py
+++ user/views.py
@@ -1,2 +1,18 @@
+from rest_framework import generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = UserSerializer
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
