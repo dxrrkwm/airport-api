c427b8c08fd4c7b13b85496b4192a31f6546d3aa
Vadym Sulim <ohnooq1@gmail.com>
2024-12-30 13:17:26 +0200

    added admin settings for user app

--- user/admin.py
+++ user/admin.py
@@ -1,2 +1,11 @@
+from django.contrib import admin
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "email", "first_name", "last_name")
+    list_filter = ("is_staff",)
+    ordering = ("username",)
